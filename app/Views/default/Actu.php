86f065f06aca9aa6d11318f8f1b01dfe819bd028
jerem <jejefont-69@hotmail.fr>
2017-03-02 14:08:31 +0100

    image slider events

--- app/Views/default/Actu.php
+++ app/Views/default/Actu.php
@@ -2,8 +2,22 @@
 
 <?php $this->start('main_content') ?>
 
-	<h2>Let's code.</h2>
-	<p>ACTU.</p>
+<p class="titre">Actualités</p>
+
+<fieldset>
+    <legend>Top actus</legend>
+    <div class="col-md-12">
+
+        <p class="col-md-4 text-left">TITRE</p>
+
+        <p class="col-md-12 text-center">Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
+
+
+    </div>
+
+
+
+</fieldset>
 
 
 <?php $this->stop('main_content') ?>
